4c6fdbecc7f51e865db073973cffbf52b17a9b6a
Rkknoob <iboonkhet@gmail.com>
2023-07-05 08:51:11 +0700

    fisrt

---
+++ layouts/default.vue
@@ -0,0 +1,38 @@
+<script lang="ts" setup>
+
+import Head from '@/components/layout/Head.vue';
+import Sidebar from '@/components/layout/Sidebar.vue';
+
+
+</script>
+<template>
+
+   
+
+
+
+
+
+    <Head></Head>
+    <!--  END NAVBAR  -->
+
+    <!--  BEGIN MAIN CONTAINER  -->
+    <div class="main-container" id="container">
+
+        <div class="overlay"></div>
+        <div class="search-overlay"></div>
+
+        <!--  BEGIN SIDEBAR  -->
+        <Sidebar></Sidebar>
+        <!--  END SIDEBAR  -->
+
+        <!--  BEGIN CONTENT AREA  -->
+
+           <NuxtPage />
+       
+
+    </div>
+
+</template>
+
+<style scoped></style>
---
+++ nuxt.config.ts
@@ -0,0 +1,39 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  devtools: { enabled: false },
+
+  css: ['~/assets/src/bootstrap/css/bootstrap.min.css',
+  '~/assets/src/bootstrap/css/bootstrap.min.css',
+  '~/assets/layouts/modern-light-menu/css/light/plugins.css',
+  '~/assets/src/plugins/src/apex/apexcharts.css',
+  '~/assets/src/assets/css/light/dashboard/dash_1.css',
+  '~/assets/src/assets/css/dark/dashboard/dash_1.css',
+  '~/assets/layouts/modern-light-menu/css/light/loader.css',
+  '~/assets/layouts/modern-light-menu/css/dark/loader.css'
+],
+
+modules: [
+  
+],
+app:{
+
+  head: {
+    script: [
+      { src: '/src/bootstrap/js/bootstrap.bundle.min.js', defer: true },
+      { src: '/layouts/modern-light-menu/app.js', defer: true },
+      { src: '/src/plugins/src/perfect-scrollbar/perfect-scrollbar.min.js', defer: true },
+      { src: '/src/plugins/src/mousetrap/mousetrap.min.js', defer: true },
+      { src: '/src/plugins/src/waves/waves.min.js', defer: true },
+      { src: '/src/plugins/src/apex/apexcharts.min.js', defer: true },
+      { src: '/src/assets/js/dashboard/dash_1.js', defer: true }
+    ]
+  },
+},
+
+
+vite: {
+  define: {
+    "process.env.DEBUG": false,
+  },
+},
+})
