43c3ae986efbb33674a91d3d7d62434df1943cb1
iboonkhet@gmail.com <123219570+Rkknoob1@users.noreply.github.com>
2023-07-26 17:20:38 +0700

    update
    update

---
+++ pages/exam/index.vue
@@ -0,0 +1,67 @@
+
+
+
+<script setup lang="ts">
+// import DataTable from 'datatables.net-vue3';
+// import DataTablesCore from 'datatables.net-bs5';
+import { storeToRefs } from 'pinia';
+import { defineComponent } from 'vue';
+import { CourseStore } from '@/store/course'
+import ExamList from '@/components/exam/ExamList.vue'
+import { useToast } from 'vue-toastification';
+
+const toast = useToast()
+const store = CourseStore()
+
+
+
+
+</script>
+
+<template>
+
+  <div id="content" class="main-content">
+            <div class="layout-px-spacing">
+              <div class="page-meta">
+                        <nav class="breadcrumb-style-one" aria-label="breadcrumb">
+                            <ol class="breadcrumb">
+                                <li class="breadcrumb-item"><a href="#">Exam </a></li>
+                                <li class="breadcrumb-item active" aria-current="page">Exam List</li>
+                            </ol>
+                        </nav>
+                    </div>
+            <ExamList></ExamList>
+            </div>
+          </div>
+
+
+    
+    
+
+       
+</template>
+
+<style>
+.modal {
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background-color: rgba(0, 0, 0, 0.5);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.modal-content {
+  background-color: white;
+  padding: 20px;
+  width: 50%;
+}
+
+button {
+  margin-top: 10px;
+}
+
+</style>
