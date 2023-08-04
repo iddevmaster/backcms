58fb3c9d28b727af1fe96f1e511030d78f2c4bc0
Rkknoob <iboonkhet@gmail.com>
2023-08-04 23:57:20 +0700

    update
    update

--- store/exam.ts
+++ store/exam.ts
@@ -6,47 +6,47 @@ const user_id = useCookie('user_id');
 
 
 
-export const ExamStore = defineStore('exam', {  
+export const ExamStore = defineStore('exam', {
   state: () => ({
-    isOpen:false,
-    isOpenCreate:false,
-    isOpenEdit:false,
-    image:null,
+    isOpen: false,
+    isOpenCreate: false,
+    isOpenEdit: false,
+    image: null,
     sortedbyASC: true,
-    imageReq:false,
-    imagelist:null,
-    total_page:null,
-limit_page:null,
-current_page:null,
-total_filter:null,
-total:null,
-    path:"",
-    examlist:[],
-    exam:null,
+    imageReq: false,
+    imagelist: null,
+    total_page: null,
+    limit_page: null,
+    current_page: null,
+    total_filter: null,
+    total: null,
+    path: "",
+    examlist: [],
+    exam: null,
     formexam: {
       em_code: "",
       em_name: "",
       em_cover: "",
       em_description: "",
       em_random_amount: null,
-      em_time:ref({
+      em_time: ref({
         hours: '00',
         minutes: '59',
-        seconds:'00'
+        seconds: '00'
       }),
       user_id: user_id.value
     },
     formexamedit: {
-      em_id:"",
+      em_id: "",
       em_code: "",
       em_name: "",
       em_cover: "",
       em_description: "",
       em_random_amount: null,
-      em_time:ref({
+      em_time: ref({
         hours: '00',
         minutes: '59',
-        seconds:'00'
+        seconds: '00'
       }),
       user_id: user_id.value
     },
@@ -55,9 +55,9 @@ total:null,
       per_page: 10,
       search: '',
     },
-}
-     
-),
+  }
+
+  ),
 
 
 
@@ -84,192 +84,193 @@ total:null,
 
 
   },
-  
+
   actions: {
-  async fetchExamlist() {
+    async fetchExamlist() {
 
-await this.ResetForm();
-    try {
-      const data = await ApiService.post('/exam/main/list', this.formsearchexam).then(response => {
-       this.examlist = response.data.data   
-       this.total_page = response.data.total_page
-       this.limit_page = response.data.limit_page
-       this.current_page = response.data.current_page
-       this.total_filter = response.data.total_filter
-       this.total = response.data.total
-      });
-
-    } catch (error) {
-      console.log('error');
-      return false;
-    } finally {
-
-    }
-
-  },
-
-  sortLists(sortBy) {
-    if (this.sortedbyASC) {
-      this.examlist.sort((x, y) => (x[sortBy] > y[sortBy] ? -1 : 1));
-      this.sortedbyASC = false;
-    } else {
-      this.examlist.sort((x, y) => (x[sortBy] < y[sortBy] ? -1 : 1));
-      this.sortedbyASC = true;
-    }
-  },
-
-  async selectentires(data_entires) {
-
-    this.formsearchexam.per_page = data_entires;
-    this.formsearchexam.page = 1;
-    },
-  setCurrentPage(page) {
-    this.formsearchexam.page = page
-  },
-  async SaveExam() {
-  
-this.ChangeFormateTime('add');
-    try {
-      const data = await ApiService.post('/exam/main/create', this.formexam).then(response => {
-       return true;
-      });
-    } catch (error) {
-      return false
-    } finally {
-      this.closeModal();
-    }
-
-
-  },
-
-  async UpdateExam(){
-    this.isOpenEdit = false;
-     await this.ChangeFormateTime('edit');
-     console.log(this.formexamedit);
-    try {
-      const updateexam = await ApiService.put('/exam/main/update/' + this.formexamedit.em_id, this.formexamedit);
-      return true;
-    } catch (error) {
-      return false;
-    } finally {
-      this.closeModal();
-    }
-  },
-
-  async deleteExam(item) {
-    try {
-      await this.deleteImage(item.em_cover);
-      const del = await ApiService.delete('/exam/main/delete/' + item.em_id);
-      this.isOpen = false;
-      return true;
-    } catch (error) {
-      return false;
-    }
-
-  },
-
-  async deleteImage(image) {
-    const del = await ApiService.delete('/media_file/file/?f=' + image);
-    return true;
-  },
-
-  async ResetForm() {  
-
-this.image = null;
-this.imagelist = null;
-this.path = "";
-    this.formexam = {
-      em_code: '',
-      em_name: '',
-      em_cover: '',
-      em_description: '',
-      em_random_amount: null,
-      em_time:ref({
-        hours: '00',
-        minutes: '59',
-        seconds:'00'
-      }),
-      user_id: user_id.value,
-    };
-
-  },
-
-
-
-  async OpenFormInser() {
-    this.image = null;
-    this.isOpenCreate = true;
-  },
-  async OpenEditItem(item) {
-   
-    const timeedit = this.getDateFromHours(item.em_time);
-this.formexamedit.em_id = item.em_id
-this.formexamedit.em_code = item.em_code
-this.formexamedit.em_name = item.em_name
-this.formexamedit.em_description = item.em_description
-this.formexamedit.em_cover = item.em_cover
-this.formexamedit.em_time = timeedit
-this.formexamedit.em_random_amount = item.em_random_amount
-this.formexamedit.user_id = user_id.value,
-this.image = item.em_cover
-this.isOpenEdit = true;
-  },
-
-  async Openmodaldelete(item) {
-  
-    this.isOpen = true;
-    this.exam = item;
-  },
-
-  closeModal() {
-    this.isOpenCreate = false;
-    this.isOpenEdit = false;
-    
-  },
-
-  getDateFromHours(time) {
-  
-  const timed = time.split(':');
-  const timeedit = ref({hours: timed[0],
-  minutes: timed[1],
-  seconds:timed[2]
-  })
-  return timeedit;
-
-},
-
-  async UploadfileExam() {  
-  
-    let formData = new FormData();
-    formData.append('files', this.imagelist);
-    if(this.imagelist){
+      await this.ResetForm();
       try {
-        const data = await ApiService.upload('/media_file/upload/file',formData);
-      //  this.path = data.data[0].path
-      this.formexam.em_cover = data.data[0].path
-        this.formexamedit.em_cover = data.data[0].path
+        const data = await ApiService.post('/exam/main/list', this.formsearchexam).then(response => {
+          this.examlist = response.data.data
+          this.total_page = response.data.total_page
+          this.limit_page = response.data.limit_page
+          this.current_page = response.data.current_page
+          this.total_filter = response.data.total_filter
+          this.total = response.data.total
+        });
+
+      } catch (error) {
+        console.log('error');
+        return false;
+      } finally {
+
+      }
+
+    },
+
+    sortLists(sortBy) {
+      if (this.sortedbyASC) {
+        this.examlist.sort((x, y) => (x[sortBy] > y[sortBy] ? -1 : 1));
+        this.sortedbyASC = false;
+      } else {
+        this.examlist.sort((x, y) => (x[sortBy] < y[sortBy] ? -1 : 1));
+        this.sortedbyASC = true;
+      }
+    },
+
+    async selectentires(data_entires) {
+
+      this.formsearchexam.per_page = data_entires;
+      this.formsearchexam.page = 1;
+    },
+    setCurrentPage(page) {
+      this.formsearchexam.page = page
+    },
+    async SaveExam() {
+
+      this.ChangeFormateTime('add');
+      try {
+        const data = await ApiService.post('/exam/main/create', this.formexam).then(response => {
+          return true;
+        });
+      } catch (error) {
+        return false
+      } finally {
+        this.closeModal();
+      }
+
+
+    },
+
+    async UpdateExam() {
+      this.isOpenEdit = false;
+      await this.ChangeFormateTime('edit');
+      console.log(this.formexamedit);
+      try {
+        const updateexam = await ApiService.put('/exam/main/update/' + this.formexamedit.em_id, this.formexamedit);
         return true;
+      } catch (error) {
+        return false;
+      } finally {
+        this.closeModal();
+      }
+    },
+
+    async deleteExam(item) {
+      try {
+        await this.deleteImage(item.em_cover);
+        const del = await ApiService.delete('/exam/main/delete/' + item.em_id);
+        this.isOpen = false;
+        return true;
+      } catch (error) {
+        return false;
+      }
+
+    },
+
+    async deleteImage(image) {
+      const del = await ApiService.delete('/media_file/file/?f=' + image);
+      return true;
+    },
+
+    async ResetForm() {
+
+      this.image = null;
+      this.imagelist = null;
+      this.path = "";
+      this.formexam = {
+        em_code: '',
+        em_name: '',
+        em_cover: '',
+        em_description: '',
+        em_random_amount: null,
+        em_time: ref({
+          hours: '00',
+          minutes: '59',
+          seconds: '00'
+        }),
+        user_id: user_id.value,
+      };
+
+    },
+
+
+
+    async OpenFormInser() {
+      this.image = null;
+      this.isOpenCreate = true;
+    },
+    async OpenEditItem(item) {
+
+      const timeedit = this.getDateFromHours(item.em_time);
+      this.formexamedit.em_id = item.em_id
+      this.formexamedit.em_code = item.em_code
+      this.formexamedit.em_name = item.em_name
+      this.formexamedit.em_description = item.em_description
+      this.formexamedit.em_cover = item.em_cover
+      this.formexamedit.em_time = timeedit
+      this.formexamedit.em_random_amount = item.em_random_amount
+      this.formexamedit.user_id = user_id.value,
+        this.image = item.em_cover
+      this.isOpenEdit = true;
+    },
+
+    async Openmodaldelete(item) {
+
+      this.isOpen = true;
+      this.exam = item;
+    },
+
+    closeModal() {
+      this.isOpenCreate = false;
+      this.isOpenEdit = false;
+
+    },
+
+    getDateFromHours(time) {
+
+      const timed = time.split(':');
+      const timeedit = ref({
+        hours: timed[0],
+        minutes: timed[1],
+        seconds: timed[2]
+      })
+      return timeedit;
+
+    },
+
+    async UploadfileExam() {
+
+      let formData = new FormData();
+      formData.append('files', this.imagelist);
+      if (this.imagelist) {
+        try {
+          const data = await ApiService.upload('/media_file/upload/file', formData);
+          //  this.path = data.data[0].path
+          this.formexam.em_cover = data.data[0].path
+          this.formexamedit.em_cover = data.data[0].path
+          return true;
         } catch (error) {
           return false;
-        } 
-    }
+        }
+      }
+    },
+
+    async ChangeFormateTime(item) {
+
+
+      if (item == 'add') {
+        const time = this.formexam.em_time.hours + ':' + this.formexam.em_time.minutes + ':' + this.formexam.em_time.seconds
+        this.formexam.em_time = time;
+
+      } else {
+        const time = this.formexamedit.em_time.hours + ':' + this.formexamedit.em_time.minutes + ':' + this.formexamedit.em_time.seconds
+        this.formexamedit.em_time = time;
+
+      }
+    },
   },
 
-  async ChangeFormateTime(item) {
-    
-  
-    if(item == 'add'){
-      const time =   this.formexam.em_time.hours + ':' + this.formexam.em_time.minutes + ':' + this.formexam.em_time.seconds
-      this.formexam.em_time = time;
-
-    }else{
-      const time = this.formexamedit.em_time.hours + ':' + this.formexamedit.em_time.minutes + ':' + this.formexamedit.em_time.seconds
-      this.formexamedit.em_time = time;
-      
-    }
-  },
-  },
 
 
-  
 });
--- store/examquestion.ts
+++ store/examquestion.ts
@@ -6,46 +6,46 @@ const user_id = useCookie('user_id');
 
 
 
-export const ExamquestionStore = defineStore('examquestion', {  
+export const ExamquestionStore = defineStore('examquestion', {
   state: () => ({
-    isOpen:false,
-    isOpenCreate:false,
-    isOpenEdit:false,
-    image:null,
-   
+    isOpen: false,
+    isOpenCreate: false,
+    isOpenEdit: false,
+    image: null,
+
     sortedbyASC: true,
-    imageReq:false,
-    imagelist:null,
-    total_page:null,
-limit_page:null,
-current_page:null,
-total_filter:null,
-total:null,
-choicelist:[],
-formExamq: {
-  eq_name: "",
-  eq_image: "",
-  eq_answer: null,
-  em_id : 40,
-  id : 0,
-},
-formChoice: {
-  ec_index: null,
-  ec_name: "",
-  ec_image: "",
-  eq_id: null,
-},
-    path:"",
-    examlist:[],
-    exam:null,
+    imageReq: false,
+    imagelist: null,
+    total_page: null,
+    limit_page: null,
+    current_page: null,
+    total_filter: null,
+    total: null,
+    choicelist: [],
+    formExamq: {
+      eq_name: "",
+      eq_image: "",
+      eq_answer: null,
+      em_id: 40,
+      id: 0,
+    },
+    formChoice: {
+      ec_index: null,
+      ec_name: "",
+      ec_image: "",
+      eq_id: null,
+    },
+    path: "",
+    examlist: [],
+    exam: null,
     formsearchexamquestion: {
       page: 1,
       per_page: 10,
       search: '',
     },
-}
-     
-),
+  }
+
+  ),
 
 
   getters: {
@@ -55,158 +55,158 @@ formChoice: {
 
 
   },
-  
+
   actions: {
-  async fetchExamlist() {
+    async fetchExamlist() {
 
 
-  },
+    },
 
-  sortLists(sortBy) {
+    sortLists(sortBy) {
 
-  },
+    },
 
-  async selectentires(data_entires) {
-
- 
-  },
-  setCurrentPage(page) {
-    this.formsearchexam.page = page
-  },
-  async SaveExamq() {
-
-    try {
-      const data = await ApiService.post('/exam/question/create', this.formExamq).then(response => {
-        console.log(response.data.insertId);
-         this.SaveChoice(response.data.insertId)
-        return true;
-      });
-    } catch (error) {
-      return false;
-   
-    } finally {
-    }
-  
-  },
-  async SaveChoice(id) {
-   
-  
-    for (var i = 0; i < this.choicelist.length; i++) {
-      const x = i+1;
-      this.formChoice.ec_index = x;
-      this.formChoice.ec_name = this.choicelist[i].ec_name
-      this.formChoice.ec_image = this.choicelist[i].ec_image
-      this.formChoice.eq_id = id
-      await this.delay(500);
-      const data = await ApiService.post('/exam/choice/create', this.formChoice);
-   {
-     }
-   }
-
-   await this.ResetForm();
-
-  },
-  delay(ms) {
-    return new Promise((resolve) => setTimeout(resolve, ms));
-  },
-  async UpdateExamq(){
-
-  },
+    async selectentires(data_entires) {
 
 
+    },
+    setCurrentPage(page) {
+      this.formsearchexam.page = page
+    },
+    async SaveExamq() {
 
-  async deleteExamq(item) {
+      try {
+        const data = await ApiService.post('/exam/question/create', this.formExamq).then(response => {
+          console.log(response.data.insertId);
+          this.SaveChoice(response.data.insertId)
+          return true;
+        });
+      } catch (error) {
+        return false;
+
+      } finally {
+      }
+
+    },
+    async SaveChoice(id) {
 
 
-  },
+      for (var i = 0; i < this.choicelist.length; i++) {
+        const x = i + 1;
+        this.formChoice.ec_index = x;
+        this.formChoice.ec_name = this.choicelist[i].ec_name
+        this.formChoice.ec_image = this.choicelist[i].ec_image
+        this.formChoice.eq_id = id
+        await this.delay(500);
+        const data = await ApiService.post('/exam/choice/create', this.formChoice);
+        {
+        }
+      }
 
-  async deleteImage(image) {
- 
-  },
+      await this.ResetForm();
 
-  async ResetForm() {  
+    },
+    delay(ms) {
+      return new Promise((resolve) => setTimeout(resolve, ms));
+    },
+    async UpdateExamq() {
+
+    },
 
 
-    this.choicelist = [];
-    this.image = null,
-    this.imageReq = false,
-    this.imagelist = null,
-    this.formExamq = {
+
+    async deleteExamq(item) {
+
+
+    },
+
+    async deleteImage(image) {
+
+    },
+
+    async ResetForm() {
+
+
+      this.choicelist = [];
+      this.image = null,
+        this.imageReq = false,
+        this.imagelist = null,
+        this.formExamq = {
           eq_name: "",
           eq_image: "",
           eq_answer: null,
           em_id: 40,
-          id : 0,
+          id: 0,
         };
-    
-      },
 
-  async AdChoice(){
-    var x = this.choicelist.length + 1
-    const daa = {
-      id:x,
-      ec_index:null,
-      ec_name:"-",
-      ec_image:"",
-      eq_id:null,
-    }
-    this.choicelist.push(daa);
     },
 
-  async deleteChoice(x){
-  //  const index = this.choicelist[item]
-  const index = this.choicelist.findIndex(item => item.id === x)
+    async AdChoice() {
+      var x = this.choicelist.length + 1
+      const daa = {
+        id: x,
+        ec_index: null,
+        ec_name: "-",
+        ec_image: "",
+        eq_id: null,
+      }
+      this.choicelist.push(daa);
+    },
+
+    async deleteChoice(x) {
+      //  const index = this.choicelist[item]
+      const index = this.choicelist.findIndex(item => item.id === x)
       if (index !== -1) {
         this.choicelist.splice(index, 1)
       }
-  },
+    },
 
 
-  async OpenFormInser() {
+    async OpenFormInser() {
 
-  },
-  async OpenEditItem() {
-   
-
-  },
+    },
+    async OpenEditItem() {
 
 
-  closeModal() {
-    this.isOpenCreate = false;
-    this.isOpenEdit = false;
-    
-  },
+    },
+
+
+    closeModal() {
+      this.isOpenCreate = false;
+      this.isOpenEdit = false;
+
+    },
 
 
 
-  async UploadfileExamq() {  
-    let formData = new FormData();
-    formData.append('files', this.imagelist);
-    if(this.imagelist){
-      try {
-        const data = await ApiService.upload('/media_file/upload/file',formData);
-      //  this.path = data.data[0].path
-      this.formExamq.eq_image = data.data[0].path
-        return true;
+    async UploadfileExamq() {
+      let formData = new FormData();
+      formData.append('files', this.imagelist);
+      if (this.imagelist) {
+        try {
+          const data = await ApiService.upload('/media_file/upload/file', formData);
+          //  this.path = data.data[0].path
+          this.formExamq.eq_image = data.data[0].path
+          return true;
         } catch (error) {
           return false;
-        } 
-    }
-   
+        }
+      }
+
+    },
+
+    async uploadfileexam(x) {
+      const data = await ApiService.upload('/media_file/upload/file', x);
+      return data;
+    },
+
+    async ChangeFormateTime() {
+
+
+
+    },
   },
 
-  async uploadfileexam(x){
-    const data = await ApiService.upload('/media_file/upload/file',x);
-    return data;
-  },
-
-  async ChangeFormateTime() {
-    
-  
-
-  },
-  },
 
 
-  
 });
