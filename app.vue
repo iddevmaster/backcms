887aec6399c6115d2f2342c7289935ae82aae335
iboonkhet@gmail.com <iboonkhet@gmail.com>
2023-08-17 15:53:53 +0700

    update
    update

--- app.vue
+++ app.vue
@@ -3,6 +3,16 @@
 
   </template>
 
+<script setup lang="ts">
+useSeoMeta({
+  title: 'CMS Site',
+  ogTitle: 'CMS Site',
+  description: 'This is my CMS site.',
+  ogDescription: 'This is my CMS site',
+  ogImage: 'https://example.com/image.png',
+  twitterCard: 'summary_large_image',
+})
+</script>
 
 
 
