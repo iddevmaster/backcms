1bd58d0eae1130515798b959c6f01fafbe4c45a4
iboonkhet@gmail.com <iboonkhet@gmail.com>
2023-08-22 15:26:52 +0700

    Update CourseList.vue

--- components/course/CourseList.vue
+++ components/course/CourseList.vue
@@ -31,7 +31,7 @@
       class="col-xxl-3 col-xl-3 col-lg-3 col-md-4 col-sm-6 mb-4"
       v-for="item in store.courselist"
       :key="item.course_id"
-    >
+    > <div class="gridarea__img">
       <a class="card style-2 mb-md-0 mb-4">
         <img
           :src="image(item.course_cover)"
@@ -57,7 +57,6 @@
                                                                 <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-edit-2"><path d="M17 3a2.828 2.828 0 1 1 4 4L7.5 20.5 2 22l1.5-5.5L17 3z"></path></svg>
                                                             </a>
                                                           </NuxtLink>
-
                                                             <a href="javascript:void(0);" class="action-btn btn-delete bs-tooltip" @click="del(item)" data-toggle="tooltip" data-placement="top" aria-label="Delete" data-bs-original-title="Delete">
                                                                 <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-trash-2"><polyline points="3 6 5 6 21 6"></polyline><path d="M19 6v14a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2V6m3 0V4a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v2"></path><line x1="10" y1="11" x2="10" y2="17"></line><line x1="14" y1="11" x2="14" y2="17"></line></svg>
                                                             </a>
@@ -65,12 +64,9 @@
           </div>
         </div>
       </a>
-      
     </div>
-    
-    
+    </div>
   </div>
-
 
   <div class="paginating-container" v-if="store.total_page > 1">
       <div class="dataTables_paginate paging_simple_numbers" id="zero-config_paginate">
@@ -198,4 +194,10 @@ function image(i) {
 .dt--pagination {
   float: right;
 }
+
+ .gridarea__img img {
+    object-fit: cover;
+    height: 200px;
+    width: 100%;
+ }
 </style>
