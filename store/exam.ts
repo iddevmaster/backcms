33066e6bf63031edfdb27d8b512d5d615ac65760
iboonkhet <iboonkhet@gmail.com>
2024-03-06 10:54:43 +0700

    update
    update

--- store/exam.ts
+++ store/exam.ts
@@ -64,85 +64,94 @@ export const ExamStore = defineStore('exam', {
      dtl:[
       {
         dlt_code: "A",
-        dlt_description:
-          "ใบอนุญาตขับรถประเภท A รถจักรยานยนต์สองล้อ เครื่องยนต์ไม่เกิน 125 cc.",
         dlt_description_loas:
-          "ໃບຂັບຂີ່ປະເພດ A, ລົດຈັກສອງລໍ້ ເຄື່ອງຈັກບໍ່ເກີນ 125 ຊີຊີ.",
+          "ລົດຈັກສອງລໍ້ ຄວາມແຮງບໍ່ເກີນ 125 cc",
         dlt_description_english:
-          "Driving license, type A, two -wheel motorcycle Engine not more than 125 cc.",
-          
+          "Two-wheeled motorcycle, not too strong", 
       },
       {
         dlt_code: "A1",
-        dlt_description:
-          "ใบอนุญาตขับรถประเภท A1 รถจักรยานยนต์สองล้อ เครื่องยนต์ 125 cc. ขึ้นไป",
         dlt_description_loas:
-          "ໃບຂັບຂີ່ປະເພດ A1, ລົດຈັກສອງລໍ້, ເຄື່ອງຈັກ 125 ຊີຊີ ຫຼື ຫຼາຍກວ່ານັ້ນ.",
+          "ລົດຈັກສອງລໍ້ ຄວາມແຮງ ແຕ່ 125 cc ຂື້ນໄປ",
         dlt_description_english:
-          "Driving license, type A1, two -wheel motorcycle, 125 cc.",    
+          "Two-wheeled motorcycle with a capacity of 125 cc ",    
       },
       {
         dlt_code: "A2",
-        dlt_description:
-          "ใบอนุญาตขับรถประเภท A2 รถสามล้อส่วนตัว, รถสองล้อ และรถสามล้อโดยสาร",
           dlt_description_loas:
-          "ໃບອະນຸຍາດຂັບຂີ່ປະເພດ A2 ສາມລໍ້ເອກະຊົນ, ພາຫະນະສອງລໍ້, ແລະສາມລໍ້ຜູ້ໂດຍສານ.",
+          "ລົດຈັກສາມລໍ້ສ່ວນຕົວ, ລົດສອງລໍ້ ແລະ ສາມລໍ້ໂດຍສານ",
         dlt_description_english:
-          "Driving license, type A2, private tricycle, two -wheeled vehicles and tricycle",   
+          "Private tricycles, two-wheelers and passenger tricycles",   
       },
       {
         dlt_code: "A3",
-        dlt_description:
-          "ใบอนุญาตขับรถประเภท A3 รถแทรกเตอร์แบบมีล้อ และรถปราบดิน",
           dlt_description_loas:
-          "ໃບ​ອະ​ນຸ​ຍາດ​ໃຫ້​ຂັບ Class A3​, ລົດ​ໄຖ​ນາ​ລໍ້​ແລະ bulldozers​.",
+          "ລົດໄຖນາທີ່ແກ່ລໍ້ ແລະ ລົດຕ໋ອກໆ",
         dlt_description_english:
-          "A3 driving license with wheel tractor and soil suppression vehicles",   
+          "Old wheeled tractors and trucks",   
       },
       {
         dlt_code: "B",
-        dlt_description:
-          "ใบอนุญาตขับรถประเภท B รถยนต์ที่น้าหนักรวมน้อยกว่า 3,500 กิโลกรัม ไม่เกิน 9 ที่นั่ง รวมผู้ขับรถ",
           dlt_description_loas:
-          "ບຂັບຂີ່ປະເພດ B: ພາຫະນະທີ່ມີນໍ້າໜັກທັງໝົດບໍ່ເກີນ 3,500 ກິໂລກຣາມ, ບໍ່ເກີນ 9 ບ່ອນນັ່ງ ລວມທັງຜູ້ຂັບຂີ່.",
+          "ລົດເບົາ ສີ່ລໍ້ ທີ່ມີນ້ຳໜັກລວມບໍ່ເກີນ 3.500 ກິໂລກຣາມ ແລະ ບໍ່ເກີນ 9 ບ່ອນນັ່ງ ລວມທັງຜູ້ຂັບຂີ່",
         dlt_description_english:
-          "Driving license B type, a car with a heavy water, less than 3,500 kilograms, not more than 9 seats, including the driver",   
+          "less than 3,500 kilograms, not more than 9 seats, including the driver",   
       },
       {
         dlt_code: "C",
-        dlt_description:
-          "ใบอนุญาตขับรถประเภท C รถขนส่งสินค้าน้าหนักรวมตั้งแต่ 3,500 ถึง 7,500 กิโลกรัม",
           dlt_description_loas:
-          "ປະເພດໃບຂັບຂີ່ C1, ພາຫະນະຂົນສົ່ງສິນຄ້າທີ່ມີນໍ້າໜັກລວມແຕ່ 7,500 – 15,000 ກິໂລກຣາມ.",
+          "ລົດຂົນສົ່ງສິນຄ້ານ້ຳໜັກລວມ ແຕ່ 3.500 ກິໂລກຣາມ ຫາ 7.500 ກິໂລກຣາມ",
         dlt_description_english:
-          "Driving license C1, a total of 7,500 to 15,000 kilograms",   
+          "Heavy goods vehiclesa= total of 3,500 to 7.500 kilograms",
       },
       {
         dlt_code: "C1",
-        dlt_description:
-          "ใบอนุญาตขับรถประเภท C1 รถขนส่งสินค้าน้าหนักรวมตั้งแต่ 7,500 ถึง 15,000 กิโลกรัม",
           dlt_description_loas:
-          "ປະເພດໃບຂັບຂີ່ C1, ພາຫະນະຂົນສົ່ງສິນຄ້າທີ່ມີນໍ້າໜັກລວມແຕ່ 7,500 – 15,000 ກິໂລກຣາມ.",
+          "ລົດຂົນສົ່ງສິນຄ້ານ້ຳໜັກລວມ ເກີນກວ່າ 7.500ກິໂລກຣາມ ຫາ 15.000 ກິໂລກຣາມ",
         dlt_description_english:
-          "Driving license C1, a total of 7,500 to 15,000 kilograms",   
+          "Weight of the truck exceeds a total of 7,500 to 15,000 kilograms",   
       },
       {
         dlt_code: "C2",
-        dlt_description:
-          "ใบอนุญาตขับรถประเภท C2 รถขนส่งสินค้า น้าหนักรวม 15,000 กิโลกรัม ขึ้นไป",
           dlt_description_loas:
-          "ໃບຂັບຂີ່ປະເພດ C2, ພາຫະນະຂົນສົ່ງສິນຄ້າ, ນ້ຳໜັກທັງໝົດ 15,000 ກິໂລກຣາມຂຶ້ນໄປ.",
+          "ລົດຂົນສົ່ງສິນຄ້ານ້ຳໜັກລວມ ເກີນກວ່າ 15.000 ກິໂລກຣາມ.",
         dlt_description_english:
-          "Driving license, C2, transportation vehicles, weighing 15,000 kilograms or more",   
+          "Transportation vehicles, weighing 15,000 kilograms or more",   
       },
       {
         dlt_code: "D",
-        dlt_description:
-          "ใบอนุญาตขับรถประเภท D รถขนส่งผู้โดยสาร ประเภท 4 ล้อขึ้นไป ไม่เกิน 15 ที่นั่ง",
           dlt_description_loas:
-          "ໃບຂັບຂີ່ປະເພດ D, ພາຫະນະຂົນສົ່ງຜູ້ໂດຍສານ, 4 ລໍ້ຂຶ້ນໄປ, ບໍ່ເກີນ 15 ບ່ອນນັ່ງ.",
+          "ລົດຂົນສົ່ງຜູ້ໂດຍສານປະເພດ 4 ລໍ້ ຂຶ້ນໄປ, ບໍ່ເກີນ ສິບຫ້າ ບ່ອນນັ່ງ",
         dlt_description_english:
-          "Driving license, type D, a 4 -wheel passenger car, no more than 15 seats",   
+          "Passenger car type 4 wheels and above, no more than fifteen seats",   
+      },
+      {
+        dlt_code: "D1",
+          dlt_description_loas:
+          "ລົດຂົນສົ່ງຜູ້ໂດຍສານ ແຕ່ ສິບຫົກ ຫາ ສາມສິບຫ້າ ບ່ອນນັ່ງ",
+        dlt_description_english:
+          "Passenger cars from sixteen to thirty-five seats",   
+      },
+      {
+        dlt_code: "D2",
+          dlt_description_loas:
+          "ລົດຂົນສົ່ງຜູ້ໂດຍສານ ແຕ່ ສາມສິບຫົກ ບ່ອນນັ່ງ ຂຶ້ນໄປ",
+        dlt_description_english:
+          "Passenger cars with thirty-six seats or more",   
+      },
+      {
+        dlt_code: "E",
+          dlt_description_loas:
+          "ລົດເບົາ (B), ລົດຂົນສົ່ງສິນຄ້າ (C) ແລະ ລົດຂົນສົ່ງຜູ້ໂດຍສານ (D) ທີ່ແກ່ຫາງລາກນ້ຳໜັກລວມບໍ່ເກີນ 750 ກິໂລກຣາມ",
+        dlt_description_english:
+          "Light vehicles (B), cargo vehicles (C) and passenger vehicles (D) with a total weight of not more than 750 kg",   
+      },
+      {
+        dlt_code: "E1",
+          dlt_description_loas:
+          "ລົດຂົນສົ່ງສິນຄ້າ (C2) ແກ່ຫາງລາກນ້ຳໜັກລວມເກີນກວ່າ 750 ກິໂລກຣາມ",
+        dlt_description_english:
+          " Cargo vehicles (C2) with a total weight exceeding 750 kg",   
       },
     ]
   }
