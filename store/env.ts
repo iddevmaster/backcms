55f8fba50eb5dc0c0dbe6400458ef0df473052c1
iboonkhet <iboonkhet@gmail.com>
2024-04-24 09:49:14 +0700

    update
    update

--- store/env.ts
+++ store/env.ts
@@ -2,7 +2,7 @@ import { defineStore } from 'pinia'
 
 export const useEnvStore = defineStore('env', {
   state: () => ({
-    apidev: 'http://27.254.149.239:9200',
+    apidev: 'https://oasapi.iddriver.com',
     apiprod: 'https://dummyjson.com',
   })
 })
