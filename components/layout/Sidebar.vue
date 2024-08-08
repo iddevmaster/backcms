522768f8f5e2f8a7d5b89c6e243eacbe3b60b903
Rkknoob <iboonkhet@gmail.com>
2024-08-08 09:47:12 +0700

    update
    update

--- components/layout/Sidebar.vue
+++ components/layout/Sidebar.vue
@@ -330,66 +330,9 @@
             </li>
           </ul>
         </li>
-        <!-- <li class="menu" v-if="user_type == 1 || 2">
-          <a
-            href="#civil"
-            data-bs-toggle="collapse"
-            aria-expanded="false"
-            class="dropdown-toggle"
-          >
-            <div class="">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="24"
-                height="24"
-                viewBox="0 0 24 24"
-                fill="none"
-                stroke="currentColor"
-                stroke-width="2"
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                class="feather feather-airplay"
-              >
-                <path
-                  d="M5 17H4a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h16a2 2 0 0 1 2 2v10a2 2 0 0 1-2 2h-1"
-                ></path>
-                <polygon points="12 15 17 21 7 21 12 15"></polygon>
-              </svg>
+  
 
-              <span>{{ $t("menu_new_ci") }}</span>
-            </div>
-            <div>
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="24"
-                height="24"
-                viewBox="0 0 24 24"
-                fill="none"
-                stroke="currentColor"
-                stroke-width="2"
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                class="feather feather-chevron-right"
-              >
-                <polyline points="9 18 15 12 9 6"></polyline>
-              </svg>
-            </div>
-          </a>
-          <ul
-            class="collapse submenu list-unstyled"
-            id="civil"
-            data-bs-parent="#accordionExample"
-          >
-            <li>
-              <nuxt-link to="/news/civil">{{ $t("menu_new_ci") }}</nuxt-link>
-            </li>
-            <li>
-              <nuxt-link to="/news/civil/create">{{ $t("menu_new_ci_add") }}</nuxt-link>
-            </li>
-          </ul>
-        </li> -->
-
-                <li class="menu">
+                <!-- <li class="menu">
           <a
             href="#menu_dlt"
             data-bs-toggle="collapse"
@@ -427,7 +370,7 @@
             </li>
           
           </ul>
-        </li>
+        </li> -->
 <!-- 
            <li class="menu">
           <a
